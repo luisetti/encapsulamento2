7ac1f86142394c75a67e7cf9d3fbb7ceaa44fc41
luisetti <luis.s.silvafernando@gmail.com>
2023-04-11 14:08:34 +0000

    exercicio2

---
+++ Principal.java
@@ -0,0 +1,11 @@
+public class Principal {
+    
+    public static void main (String[]args){
+        Pessoa pessoa = new Pessoa();
+        pessoa.setNome("Lunna");
+        System.out.println(pessoa.getNome());
+        pessoa.setIdade(17);
+        System.out.println(pessoa.getIdade());
+    }
+    
+}
